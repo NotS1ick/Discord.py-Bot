cc89be5286484a83713833eee602d50635bb09ad
NotS1ick <69733624+NotS1ick@users.noreply.github.com>
2023-12-11 01:07:06 +0200

    Update admincog.py

--- cogs/admincog.py
+++ cogs/admincog.py
@@ -1,8 +1,10 @@
 import discord
 import aiofiles
+import asyncio
 from discord.ext import commands
 
 from discord.ext.commands import Converter, BadArgument
+
 
 class MemberConverter(Converter):
     async def convert(self, ctx, argument):
@@ -18,10 +20,12 @@ class MemberConverter(Converter):
 class Admincog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bot.warnings = {}  # Initialize warnings dictionary
+        self.bot.warnings = {}  
         self.bot.loop.create_task(self.load_warnings())
 
     async def load_warnings(self):
+        tasks = []
+
         for guild in self.bot.guilds:
             try:
                 async with aiofiles.open(f'{guild.id}.txt', mode='r') as file:
@@ -42,25 +46,28 @@ class Admincog(commands.Cog):
                             self.bot.warnings[guild.id][member_id][0] += 1
                             self.bot.warnings[guild.id][member_id][1].append((admin_id, reason))
                         except KeyError:
-                            pass  # Handle the KeyError as needed
+                            pass  
             except FileNotFoundError:
                 pass
-    
-    @commands.command()
-    @commands.has_permissions(mute_members=True)
-    async def mute(self, ctx)
-    
-    @commands.command()
-    async def unmute(self, ctx):
-        await ctx.send("Not implemented yet.")
 
-    @commands.command()
-    async def clear(self, ctx):
-        await ctx.send("Not implemented yet.")
+        await asyncio.gather(*tasks)
 
-    @commands.command()
-    async def purge(self, ctx):
-        await ctx.send("Not implemented yet.")
+        
+        self.bot.remove_command('pruge')
+    
+    def cog_unload(self):
+        pass  
+
+    
+    @commands.command(name="pruge", hidden=True)
+    @commands.has_permissions(manage_messages=True)
+    async def purge(self, ctx, amount=11):
+        amount = amount+1
+        if amount > 101:
+            await ctx.send('You cant delete more than 100 messages')
+        else:
+            await ctx.channel.purge(limit=amount)
+            await ctx.send ('Messages have been cleared')
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
@@ -102,13 +109,28 @@ class Admincog(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx, member_id: int, *, reason=None):
-        user = await self.bot.fetch_user(member_id)
-        if user:
-            await ctx.guild.unban(user, reason=reason)
-            await ctx.send(f'User {user.mention} has been unbanned for {reason}')
-        else:
-            await ctx.send("User not found.")
+    async def unban(self, ctx, *, member):
+        if not member:
+            return await ctx.send("Invalid argument. Please provide a valid username (e.g., `username`).")
+
+        banned_users = [ban_entry async for ban_entry in ctx.guild.bans()]
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if user.name == member:
+                try:
+                    await ctx.guild.unban(user)
+                    await ctx.send(f'Unbanned {user.mention}')
+                    return
+                except discord.errors.Forbidden:
+                    await ctx.send("I don't have permission to unban members.")
+                    return
+                except discord.HTTPException as e:
+                    await ctx.send(f"An error occurred during the unban: {e}")
+                    return
+
+        await ctx.send("User not found in the ban list.")
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
@@ -134,7 +156,6 @@ class Admincog(commands.Cog):
             await file.write(f'{member.id} {ctx.author.id} {reason}\n')
             
         await ctx.send(f'{member.mention} has {count} {"warning" if count == 1 else "warnings"}.')
-
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
@@ -169,6 +190,34 @@ class Admincog(commands.Cog):
         finally:
             await ctx.send(embed=embed)
 
-
+    @commands.command(aliases=['warn_remove'])
+    @commands.has_permissions(kick_members=True)
+    async def rwarn(self, ctx, member: discord.Member=None, *, reason=None):
+        if not member or not reason:
+            return await ctx.send('Incorrect command syntax. Please use the command like this: `!rwarn @user reason`')
+        
+        guild = ctx.guild
+        if guild.id not in self.bot.warnings:
+            return await ctx.send("This member has no warnings")
+        if member.id not in self.bot.warnings[guild.id]:
+            return await ctx.send("This member has no warnings")
+           
+        warnings_list = self.bot.warnings[guild.id][member.id][1]
+        new_warnings_list = [warning for warning in warnings_list if not (reason is not None and warning[1].lower() == reason.lower())]
+        
+        if len(new_warnings_list) < len(warnings_list):
+            self.bot.warnings[guild.id][member.id][0] -= 1
+            self.bot.warnings[guild.id][member.id][1] = new_warnings_list
+        
+        count = self.bot.warnings[guild.id][member.id][0]
+        
+        # Rewrite the file with the updated warnings list
+        async with aiofiles.open(f'{guild.id}.txt', mode='w') as file:
+            for member_id, warnings in self.bot.warnings[guild.id].items():
+                for admin_id, reason in warnings[1]:
+                    await file.write(f'{member_id} {admin_id} {reason}\n')
+                    
+        await ctx.send(f'{member.mention} now has {count} {"warning" if count == 1 else "warnings"}.')
+        
 def setup(bot):
     bot.add_cog(Admincog(bot))
